9d80a0e682bfca0f3e587e83cfde8ba43f08afaf
Bartosz Kita <bartosz.kita@gmail.com>
2017-01-09 00:24:49 +0000

    Update seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 20.times do
   name = Faker::Address.street_address
   cost_per_hour = Faker::Number.between(1, 10)
-  instant_booking = Faker::Boolean.boolean(0.3)
+  instant_booking = Faker::Boolean.boolean(0.6)
 
   ParkingSpace.create!(name: name,
                        cost_per_hour: cost_per_hour,
