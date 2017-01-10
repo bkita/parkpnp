ed9d7d630c4c35af771fe7e4e473173ff27f9993
Bartosz Kita <bkita@afilias.info>
2017-01-10 17:18:17 +0000

    Add test for ParkingSpaceController #show

--- spec/controllers/parking_spaces_controller_spec.rb
+++ spec/controllers/parking_spaces_controller_spec.rb
@@ -9,4 +9,13 @@ RSpec.describe ParkingSpacesController, type: :controller do
       expect(response).to have_http_status(200)
     end
   end
+
+  describe "GET #show" do
+    it "responds successfully with an HTTP 200 status code" do
+      parking = create(:parking_space)
+      get :show, id: parking.id
+      expect(response).to be_success
+      expect(response).to have_http_status(200)
+    end
+  end
 end
